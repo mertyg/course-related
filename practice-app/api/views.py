0db72bbca37243ffb7ae0d92a444331ef17ec1f3
mertyg <mertyuksekgonul@gmail.com>
2019-04-18 14:58:25 +0300

    practice-app

---
+++ practice-app/api/views.py
@@ -0,0 +1,16 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloWorldView(APIView):
+    def get(self, request):
+        return Response({
+            'message': 'Hello, stranger.'
+        })
+
+    def post(self, request):
+        name = request.data['name']
+
+        return Response({
+            'message': f'Hello, {name}.'
+        })
